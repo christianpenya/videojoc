830066b661911fa95a857ad79a9b6e5cfeac9470
Muahahas <eloibuisan@gmail.com>
2016-10-19 20:16:46 +0200

    Edited configuration of Microsoft Visual Studio. Debug and intermediate path and changed Unicode to multibyte.

---
+++ trunk/src/Videogame/Videogame.cpp
@@ -0,0 +1,81 @@
+#include <Windows.h>
+
+#define APPLICATION_NAME	"VIDEOGAME"
+
+//-----------------------------------------------------------------------------
+// Name: MsgProc()
+// Desc: The window's message handler
+//-----------------------------------------------------------------------------
+LRESULT WINAPI MsgProc( HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam )
+{
+  switch( msg )
+  {
+  case WM_DESTROY:
+    {
+      PostQuitMessage( 0 );
+      return 0;
+    }
+    break;
+  case WM_KEYDOWN:
+    {
+      switch( wParam )
+      {
+      case VK_ESCAPE:
+        //Cleanup();
+        PostQuitMessage( 0 );
+        return 0;
+        break;
+      }
+    }
+    break;
+  }//end switch( msg )
+
+  return DefWindowProc( hWnd, msg, wParam, lParam );
+}
+
+//-----------------------------------------------------------------------
+// WinMain
+//-----------------------------------------------------------------------
+int APIENTRY WinMain(HINSTANCE _hInstance, HINSTANCE _hPrevInstance, LPSTR _lpCmdLine, int _nCmdShow)
+{
+  // Register the window class
+  WNDCLASSEX wc = {	sizeof(WNDCLASSEX), CS_CLASSDC, MsgProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, APPLICATION_NAME, NULL };
+  
+  RegisterClassEx( &wc ); 
+
+  RECT rc = { 0, 0, 800, 600 };
+  AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
+
+  // Create the application's window
+  HWND hWnd = CreateWindow(APPLICATION_NAME, APPLICATION_NAME, WS_OVERLAPPEDWINDOW, 100, 100, rc.right - rc.left, rc.bottom - rc.top, NULL, NULL, wc.hInstance, NULL);
+
+  // Añadir aquí el Init de la applicacioón
+
+  ShowWindow( hWnd, SW_SHOWDEFAULT );
+  UpdateWindow( hWnd );
+  MSG msg;
+  ZeroMemory( &msg, sizeof(msg) );
+
+  // Añadir en el while la condición de salida del programa de la aplicación
+  float angle = 0.2f;
+  float height = 2;
+
+  while( msg.message != WM_QUIT )
+  {
+    if( PeekMessage( &msg, NULL, 0U, 0U, PM_REMOVE ) )
+    {
+      TranslateMessage( &msg );
+      DispatchMessage( &msg );
+    }
+    else
+    {
+       
+    }
+  }
+  
+  UnregisterClass( APPLICATION_NAME, wc.hInstance );
+
+  // Añadir una llamada a la alicación para finalizar/liberar memoria de todos sus datos
+
+  return 0;
+}
