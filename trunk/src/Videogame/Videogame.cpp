c71df93cfd2091434d99816df5d5609351dc909f
christianpenya <christianpenya@gmail.com>
2017-01-17 19:43:19 +0100

    Update de Engine y Videogame

--- trunk/src/Videogame/Videogame.cpp
+++ trunk/src/Videogame/Videogame.cpp
@@ -1,16 +1,21 @@
 #include "Utils/MemLeaks/MemLeaks.h"
 #include "Engine\Materials\MaterialManager.h"
+#include "Engine\Textures\TextureManager.h"
 #include "Engine\Render\RenderManager.h"
 #include "Engine\Input\InputManager.h"
 #include "Engine\Input\ActionManager.h"
+#include "Engine\Mesh\MeshManager.h"
+#include "Engine\Effects\ShaderManager.h"
+#include "Engine\Effects\EffectManager.h"
+#include "Engine\Effects\TechniquePoolManager.h"
+#include "Engine\Scenes\SceneManager.h"
 #include "Engine\imgui_impl_dx11.h"
-#include "Engine\SphericalCameraController.h"
-#include "Engine\FpsCameraController.h"
-#include "Engine\TpsCameraController.h"
 #include "ImGUI\imgui.h"
 #include "Engine\Engine.h"
 #include <Windows.h>
 #include <chrono>
+#include <memory>
+
 
 #define APPLICATION_NAME	"ESCAPE FROM ALCATRAZ"
 
@@ -30,45 +35,45 @@ bool debug=false;
 //-----------------------------------------------------------------------------
 LRESULT WINAPI MsgProc( HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam )
 {
-  switch( msg )
-  {
-  case WM_DESTROY:
+    switch( msg )
     {
-      PostQuitMessage( 0 );
-      return 0;
-    }
-    break;
-  case WM_KEYDOWN:
+    case WM_DESTROY:
     {
-      switch( wParam )
-      {
-      case VK_ESCAPE:
-        //Cleanup();
         PostQuitMessage( 0 );
         return 0;
-        break;
-      }
     }
-  case WM_ACTIVATE:
-  {
-	s_WindowActive = wParam != WA_INACTIVE;
-	  break;
-  }
     break;
-  case WM_SIZE:
-  {
-	  if (wParam == SIZE_MAXIMIZED)
-	  {
-		  auto& renderManager = CEngine::GetInstance().GetRenderManager();
-		  renderManager.Resize((UINT)LOWORD(lParam), (UINT)HIWORD(lParam), hWnd);
-	  }
-	  return 0;
-  }
-  break;
+    case WM_KEYDOWN:
+    {
+        switch( wParam )
+        {
+        case VK_ESCAPE:
+            //Cleanup();
+            PostQuitMessage( 0 );
+            return 0;
+            break;
+        }
+    }
+    case WM_ACTIVATE:
+    {
+        s_WindowActive = wParam != WA_INACTIVE;
+        break;
+    }
+    break;
+    case WM_SIZE:
+    {
+        if (wParam == SIZE_MAXIMIZED)
+        {
+            auto& renderManager = CEngine::GetInstance().GetRenderManager();
+            renderManager.Resize((UINT)LOWORD(lParam), (UINT)HIWORD(lParam), hWnd);
+        }
+        return 0;
+    }
+    break;
 
-  }//end switch( msg )
+    }//end switch( msg )
 
-  return DefWindowProc( hWnd, msg, wParam, lParam );
+    return DefWindowProc( hWnd, msg, wParam, lParam );
 }
 
 
@@ -78,131 +83,163 @@ LRESULT WINAPI MsgProc( HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam )
 int APIENTRY WinMain(HINSTANCE _hInstance, HINSTANCE _hPrevInstance, LPSTR _lpCmdLine, int _nCmdShow)
 {
 
-  #ifdef _DEBUG
-	MemLeaks::MemoryBegin();
-	debug=true;
-  #endif
-	
-	// Register the window class
-	WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, MsgProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, APPLICATION_NAME, NULL };
+#ifdef _DEBUG
+    MemLeaks::MemoryBegin();
+    debug=true;
+#endif
 
-	RegisterClassEx(&wc);
+    // Register the window class
+    WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, MsgProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, APPLICATION_NAME, NULL };
 
-	RECT rc = { 0, 0, m_width, m_height };
-	AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
+    RegisterClassEx(&wc);
 
-	// Create the application's window
-	HWND hWnd = CreateWindow(APPLICATION_NAME, APPLICATION_NAME, WS_OVERLAPPEDWINDOW, 0, 0, rc.right - rc.left, rc.bottom - rc.top, NULL, NULL, wc.hInstance, NULL);
+    RECT rc = { 0, 0, m_width, m_height };
+    AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
 
-	// Añadir aquí el Init de la applicacioón
-	CRenderManager l_RenderManager;
-	l_RenderManager.SetModelMatrix(m_model_matrix);
-	l_RenderManager.SetProjectionMatrix(45.0f, m_width / m_height, 0.5f, 100.0f);
-	l_RenderManager.SetViewMatrix(m_vpos, m_vtarget, m_vup);
-	l_RenderManager.Init(hWnd, m_width, m_height, debug);
-	
-	
-	CInputManager l_InputManager(hWnd);
-	CActionManager l_ActionManager(l_InputManager);
+    // Create the application's window
+    HWND hWnd = CreateWindow(APPLICATION_NAME, APPLICATION_NAME, WS_OVERLAPPEDWINDOW, 0, 0, rc.right - rc.left, rc.bottom - rc.top, NULL, NULL, wc.hInstance, NULL);
 
-	std::string inputConfigPath = "data/config/input_config.xml";
-	l_ActionManager.LoadActions(inputConfigPath);
+    // Añadir aquí el Init de la applicacioón
+    CRenderManager l_RenderManager;
+    l_RenderManager.SetModelMatrix(m_model_matrix);
+    l_RenderManager.SetProjectionMatrix(45.0f, m_width / m_height, 0.5f, 100.0f);
+    l_RenderManager.SetViewMatrix(m_vpos, m_vtarget, m_vup);
+    l_RenderManager.Init(hWnd, m_width, m_height, debug);
 
-	// Setup iMGui binding
-	ImGui_ImplDX11_Init(hWnd, l_RenderManager.GetDevice(), l_RenderManager.GetDeviceContext());
+    CInputManager l_InputManager(hWnd);
+    CActionManager l_ActionManager(l_InputManager);
 
-	// Setup Cameras
-	CSphericalCameraController l_SphericalCamera(Vect3f(0, 0, 0), 1.5f, -1.5f, 20.0f, 1.0f);
-	CFpsCameraController l_FpsCamera(Vect3f(0, 1.0, 0), 1.5f, -1.5f);
-	CTpsCameraController l_TpsCamera(Vect3f(0, 0, 0), 1.5f, -1.5f, 20.0f, 4.0f);
+    CMaterialManager l_MaterialManager;
 
-	CEngine l_Engine = CEngine();
-	l_Engine.SetActionManager(&l_ActionManager);
-	l_Engine.SetCameraController(&l_SphericalCamera);
-	l_Engine.SetRenderManager(&l_RenderManager);
-	l_Engine.fpsCam = l_FpsCamera;
-	l_Engine.orbitalCam = l_SphericalCamera;
-	l_Engine.tpsCam = l_TpsCamera;
+    std::string inputConfigPath = "data/config/input_config.xml";
+    l_ActionManager.LoadActions(inputConfigPath);
 
-	ShowWindow(hWnd, SW_SHOWDEFAULT);
-	UpdateWindow(hWnd);
-	MSG msg;
-	ZeroMemory(&msg, sizeof(msg));
-	
-	int l_prevCameraSelector = 0;
-	std::chrono::monotonic_clock l_Clock;
-	std::chrono::monotonic_clock::time_point l_PrevTime;
-	float timer = 0.0f;
-	std::string s_fps("");
-	float fps = 0.0f;
+    // Setup iMGui binding
+    ImGui_ImplDX11_Init(hWnd, l_RenderManager.GetDevice(), l_RenderManager.GetDeviceContext());
 
-	while (msg.message != WM_QUIT) 	{
+    // Setup Cameras
+    CSphericalCameraController l_SphericalCamera(Vect3f(0, 0, 0), 1.5f, -1.5f, 20.0f, 1.0f);
+    CFpsCameraController l_FpsCamera(Vect3f(0, 1.0, 0), 1.5f, -1.5f);
+    CTpsCameraController l_TpsCamera(Vect3f(0, 0, 0), 1.5f, -1.5f, 20.0f, 4.0f);
 
-		auto currentTime = l_Clock.now();
-		std::chrono::duration<float> chronoDeltaTime = currentTime - l_PrevTime;
-		l_PrevTime = currentTime;
+    CEngine& l_Engine = CEngine::GetInstance();
+    l_Engine.SetActionManager(&l_ActionManager);
+    l_Engine.SetCameraController(&l_SphericalCamera);
+    l_Engine.SetRenderManager(&l_RenderManager);
+    l_Engine.SetMaterialManager(&l_MaterialManager);
+    l_Engine.fpsCam = l_FpsCamera;
+    l_Engine.orbitalCam = l_SphericalCamera;
+    l_Engine.tpsCam = l_TpsCamera;
 
-		float dt = chronoDeltaTime.count() > 0.5f ? 0.5f : chronoDeltaTime.count();
+    ShowWindow(hWnd, SW_SHOWDEFAULT);
+    UpdateWindow(hWnd);
+    MSG msg;
+    ZeroMemory(&msg, sizeof(msg));
 
-		l_InputManager.PreUpdate(s_WindowActive);
+    int l_prevCameraSelector = 0;
+    std::chrono::monotonic_clock l_Clock;
+    std::chrono::monotonic_clock::time_point l_PrevTime;
+    float timer = 0.0f;
+    std::string s_fps("");
+    float fps = 0.0f;
 
-		while (PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE)) {
+    std::string lLevelMaterialsFilename = "reclusion.xml";
+    std::string lDefaultMaterialsFilename = "default.xml";
 
-			ImGui_ImplDX11_WndProcHandler(hWnd, msg.message, msg.wParam, msg.lParam);
-			bool fHandled = false;
+    l_MaterialManager.Load(lLevelMaterialsFilename, lDefaultMaterialsFilename);
 
-			if ((msg.message >= WM_MOUSEFIRST && msg.message <= WM_MOUSELAST) || msg.message == WM_INPUT) {
-				fHandled = l_InputManager.HandleMouse(msg);
-			}
-			else if (msg.message >= WM_KEYFIRST && msg.message <= WM_KEYLAST)
-			{
-				fHandled = l_InputManager.HandleKeyboard(msg);
-				bool WasDown = ((msg.lParam & (1 << 30)) != 0);
-				bool IsDown = ((msg.lParam & (1 << 31)) == 0);
-				bool Alt = ((msg.lParam & (1 << 29)) != 0);
+    CTextureManager* l_TextureManager = new CTextureManager();
 
-				if (!WasDown && IsDown && Alt && msg.wParam == VK_RETURN)
-				{
-					WINDOWPLACEMENT windowPosition = { sizeof(WINDOWPLACEMENT) };
-					GetWindowPlacement(msg.hwnd, &windowPosition);
+    CMeshManager* l_MeshManager = new CMeshManager();
+    l_MeshManager->GetMesh("data/meshes/Plane001.mesh");
+    //l_MeshManager->GetMesh("data/meshes/Plane002.mesh");
+    l_Engine.SetMeshManager(l_MeshManager);
 
-					l_RenderManager.ToggleFullscreen(msg.hwnd, windowPosition);
-					fHandled = true;
-				}
-				else if (msg.message == WM_KEYDOWN && msg.wParam == VK_ESCAPE)
-				{
-					PostQuitMessage(0);
-				}
-				else
-				{
-					fHandled = l_InputManager.HandleKeyboard(msg);
-				}
-			}
+    CShaderManager* l_ShaderManager = new CShaderManager();
+    l_ShaderManager->Load("data/shaders.xml");
+    l_Engine.SetShaderManager(l_ShaderManager);
 
+    CEffectManager* l_EffectManager = new CEffectManager();
+    l_EffectManager->Load("data/effects.xml");
+    l_Engine.SetEffectManager(l_EffectManager);
 
+    CTechniquePoolManager* l_TechniquePoolManager = new CTechniquePoolManager();
+    l_TechniquePoolManager->Load("data/techniques_pool.xml");
+    l_TechniquePoolManager->Apply("forward");
+    l_Engine.SetTechniquePoolManager(l_TechniquePoolManager);
 
-			if (!fHandled) {
-				TranslateMessage(&msg);
-				DispatchMessage(&msg);
-			}
+    //CSceneManager* l_SceneManager = new CSceneManager();
+    //l_SceneManager->Load("data/scenes.xml");
 
-			if (msg.message == WM_QUIT) {
-				break;
-			}
-		}
+    while (msg.message != WM_QUIT)
+    {
+        auto currentTime = l_Clock.now();
+        std::chrono::duration<float> chronoDeltaTime = currentTime - l_PrevTime;
+        l_PrevTime = currentTime;
 
-		l_InputManager.PostUpdate();
-		l_Engine.ProcessInputs();
-		l_Engine.Update();
-		l_Engine.Render();
-	}
-	
-	// Añadir una llamada a la alicación para finalizar/liberar memoria de todos sus datos
-	#ifdef _DEBUG
-		l_RenderManager.ReportLive();
-		MemLeaks::MemoryEnd();
-	#endif	
+        float dt = chronoDeltaTime.count() > 0.5f ? 0.5f : chronoDeltaTime.count();
 
-	UnregisterClass(APPLICATION_NAME, wc.hInstance);
-	return 0;
+        l_InputManager.PreUpdate(s_WindowActive);
+
+        while (PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
+        {
+
+            ImGui_ImplDX11_WndProcHandler(hWnd, msg.message, msg.wParam, msg.lParam);
+            bool fHandled = false;
+
+            if ((msg.message >= WM_MOUSEFIRST && msg.message <= WM_MOUSELAST) || msg.message == WM_INPUT)
+            {
+                fHandled = l_InputManager.HandleMouse(msg);
+            }
+            else if (msg.message >= WM_KEYFIRST && msg.message <= WM_KEYLAST)
+            {
+                fHandled = l_InputManager.HandleKeyboard(msg);
+                bool WasDown = ((msg.lParam & (1 << 30)) != 0);
+                bool IsDown = ((msg.lParam & (1 << 31)) == 0);
+                bool Alt = ((msg.lParam & (1 << 29)) != 0);
+
+                if (!WasDown && IsDown && Alt && msg.wParam == VK_RETURN)
+                {
+                    WINDOWPLACEMENT windowPosition = { sizeof(WINDOWPLACEMENT) };
+                    GetWindowPlacement(msg.hwnd, &windowPosition);
+
+                    l_RenderManager.ToggleFullscreen(msg.hwnd, windowPosition);
+                    fHandled = true;
+                }
+                else if (msg.message == WM_KEYDOWN && msg.wParam == VK_ESCAPE)
+                {
+                    PostQuitMessage(0);
+                }
+                else
+                {
+                    fHandled = l_InputManager.HandleKeyboard(msg);
+                }
+            }
+
+            if (!fHandled)
+            {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+
+            if (msg.message == WM_QUIT)
+            {
+                break;
+            }
+        }
+
+        l_InputManager.PostUpdate();
+        l_Engine.ProcessInputs();
+        l_Engine.Update();
+        l_Engine.Render();
+    }
+
+    // TODO Añadir una llamada a la alicación para finalizar/liberar memoria de todos sus datos
+#ifdef _DEBUG
+    l_RenderManager.ReportLive();
+    MemLeaks::MemoryEnd();
+#endif
+
+    UnregisterClass(APPLICATION_NAME, wc.hInstance);
+    return 0;
 }
