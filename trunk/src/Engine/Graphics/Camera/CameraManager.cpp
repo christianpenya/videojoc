ff2c110fd63c09497c8cd768113c55956667ede2
Christian <christianpenya@gmail.com>
2017-08-14 17:29:29 +0200

    ola k ase!

--- trunk/src/Engine/Graphics/Camera/CameraManager.cpp
+++ trunk/src/Engine/Graphics/Camera/CameraManager.cpp
@@ -1,10 +1,10 @@
 #include "CameraManager.h"
 
 CCameraManager::CCameraManager():
-    m_CameraSelector(1)
-    , m_PrevCameraSelector(-1)
+    m_CameraSelector(0),
+    m_PrevCameraSelector(-1)
 {
-    m_FreeCam = new CFreeCameraController(Vect3f(19.4196f, 2.36218f, 30.1499f), Vect3f(0, 0, 1), Vect3f(0, 1, 0), Vect4f(1, 500, 1.13f, 1.7f), 1.5f, -1.5f, 10.0f, -10.0f);
+    m_FreeCam = new CFreeCameraController(Vect3f(5.0f, 2.36f, 5.0f), Vect3f(0, 0, 1), Vect3f(0, 1, 0), Vect4f(1, 500, 1.13f, 1.7f), 1.5f, -1.5f, 10.0f, -10.0f);
     m_FpsCam = new CFpsCameraController(Vect3f(0, 1, 0), 1.5f, -1.5f);
     m_OrbitalCam = new CSphericalCameraController();
     m_TPSCam = new CTpsCameraController();
